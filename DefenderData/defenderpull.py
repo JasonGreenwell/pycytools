d9977904829361319647e3dc7706b66ed849e2e3
Jason Greenwell <jason.greenwell@outlook.com>
2023-01-14 17:07:15 -0700

    Add new tool

---
+++ DefenderData/defenderpull.py
@@ -0,0 +1,9 @@
+import wmi
+
+def get_defender_scan_results(computer_name):
+    computer = wmi.WMI(computer = computer_name)
+    scan_results = c.Win32_SecurityCenter()[0].AntiVirusProduct.getProductInfo()
+    return scan_results
+
+computer_name = "target_computer_name"
+print(get_defender_scan_results(computer_name))
